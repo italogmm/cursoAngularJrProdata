8e1f88729a916b78e794f299d64d727b7055d0ec
Italo Gustavo Miranda Melo <italogustavomirandamelo@Italos-MacBook-Pro.local>
2017-01-28 16:56:58 -0200

    commit segunda aula

---
+++ app/controller/index8.controller.js
@@ -0,0 +1,12 @@
+angular.module('pdCurso')
+    .controller('Index8Controller', Index8Controller);
+
+function Index8Controller($scope, $state){
+
+    $scope.alterarRota = alterarRota;
+
+    function alterarRota(state){
+        $state.go(state)
+    }
+
+}
